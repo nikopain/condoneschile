429deb1527e4e4a644e5d82f0017816230057075
Pablo Herrera <andrez.herrera@gmail.com>
2016-05-23 19:24:44 -0400

    Añadir dependencias, clase e interfaz Java para utilizar la API

---
+++ app/src/main/java/usm/cc/network/ApiClient.java
@@ -0,0 +1,20 @@
+package usm.cc.network;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient {
+
+    private static final String BASE_URL = "http://45.55.159.85/api/";
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
